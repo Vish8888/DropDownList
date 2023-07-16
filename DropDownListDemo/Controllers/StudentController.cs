6cb672fe89b6cd3ae3a496d8f00e5da57e8b76e3
Vishal Chimkar <vishal.chimkar12@gmail.com>
2023-07-16 15:49:45 +0530

    Add project files.

---
+++ DropDownListDemo/Controllers/StudentController.cs
@@ -0,0 +1,48 @@
+﻿using DropDownListDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace DropDownListDemo.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly AppDbContext _db;
+
+        public StudentController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View(_db.Students.ToList());
+        }
+
+        public IActionResult Create()
+        {
+            StudentVM student = new StudentVM()
+            {
+                student = new Student(),
+                cityList = _db.StateSamples.ToList().Select(a=>new SelectListItem()
+                {
+                    Value = a.Name.ToString(),
+                    Text = a.Name.ToString()
+                })
+            };
+
+            return View(student);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Student model)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Students.Add(model);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
---
+++ DropDownListDemo/Models/Student VM.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DropDownListDemo.Models;
+
+
+public class StudentVM
+{
+    public Student student { get; set; }
+
+    public IEnumerable<SelectListItem> cityList { get; set; }
+}
